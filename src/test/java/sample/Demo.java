0e2e51c686e288fbdee4691c7cdbd2f529ebb0c3
shara <shara@Supriya-DELL>
2023-08-03 20:41:51 +0530

    new class is committed

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -14,6 +14,14 @@ public class Demo {
     {
 	System.out.println("Hi this is my first demo1");
     }
+   @Test	
+   public void sample2()
+
+    {
+	System.out.println("Hi this is my first demo2");
+    }
+   
 }
+
 
 
---
+++ src/test/java/sample/Newclass.java
@@ -0,0 +1,10 @@
+package sample;
+import org.testng.annotations.Test;
+public class Newclass {
+
+	@Test
+	public void newclass()
+	{
+		System.out.println("this is my newclass");
+	}
+}
